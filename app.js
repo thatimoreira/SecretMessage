417088349115004a6b59c0a52e65a345d8575ff8
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-07 15:45:53 -0300

    Implemented the logic to encrypt the user text and shows it on the right part of the screen (desktops)

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-function encryptText(tag, text) {
-  let field = document.querySelector(tag);
-  field.innerHTML = text;
+function encryptText() {
+  /* RULES:
+  1. The tag should be a valid HTML
+  2. Text should not contain any special characters or letters with accents
+  8. Text should not contain lower case letters
+  */
+  let text = document.querySelector("textarea").value;
+  let encryptedText = "";
+
+  for (let i = 0; i < text.length; i++) {
+    if (
+      text[i] == "A" ||
+      text[i] == "E" ||
+      text[i] == "I" ||
+      text[i] == "O" ||
+      text[i] == "U"
+    )
+      text[i] = text[i].toLowerCase();
+    if (text[i] == "a") encryptedText += "ai";
+    else if (text[i] == "e") encryptedText += "enter";
+    else if (text[i] == "i") encryptedText += "imes";
+    else if (text[i] == "o") encryptedText += "ober";
+    else if (text[i] == "u") encryptedText += "ufat";
+    else encryptedText += text[i];
+  }
+  const userOutput = document.querySelector("#user-output");
+  userOutput.textContent += encryptedText;
 }
 
 function decryptText() {}
+
+function clearTextArea() {}
 
 function copyText() {}
