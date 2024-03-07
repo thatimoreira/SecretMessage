dc990e8e8f762cd3382d34d8d822f6a14c02f297
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-07 19:02:46 -0300

    Implemented functions to encrypt and decrypt the user input

--- app.js
+++ app.js
@@ -5,29 +5,99 @@ function encryptText() {
   8. Text should not contain lower case letters
   */
   let text = document.querySelector("textarea").value;
+
+  if (/[^a-z0-9]/.test(text)) {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent =
+      "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
+    errorMessage.classList.add("error-message");
+    const mainSection = document.querySelector(".encrypt-decrypt");
+    mainSection.insertBefore(
+      errorMessage,
+      document.querySelector(".encrypt-decrypt__user-action")
+    );
+    return;
+  }
+
   let encryptedText = "";
 
   for (let i = 0; i < text.length; i++) {
-    if (
-      text[i] == "A" ||
-      text[i] == "E" ||
-      text[i] == "I" ||
-      text[i] == "O" ||
-      text[i] == "U"
-    )
-      text[i] = text[i].toLowerCase();
-    if (text[i] == "a") encryptedText += "ai";
-    else if (text[i] == "e") encryptedText += "enter";
-    else if (text[i] == "i") encryptedText += "imes";
-    else if (text[i] == "o") encryptedText += "ober";
-    else if (text[i] == "u") encryptedText += "ufat";
-    else encryptedText += text[i];
+    if (text[i] == "a")
+        encryptedText += "ai";
+    else if (text[i] == "e")
+        encryptedText += "enter";
+    else if (text[i] == "i")
+        encryptedText += "imes";
+    else if (text[i] == "o")
+        encryptedText += "ober";
+    else if (text[i] == "u")
+        encryptedText += "ufat";
+    else
+        encryptedText += text[i];
   }
   const userOutput = document.querySelector("#user-output");
-  userOutput.textContent += encryptedText;
+  userOutput.textContent = encryptedText;
 }
 
-function decryptText() {}
+function decryptText() {
+  /* RULES:
+    1. The tag should be a valid HTML
+    2. Text should not contain any special characters or letters with accents
+    8. Text should not contain lower case letters
+    */
+    let text = document.querySelector("textarea").value;
+
+    if (/[^a-z0-9]/.test(text)) {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent =
+        "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
+        errorMessage.classList.add("error-message");
+        const mainSection = document.querySelector(".encrypt-decrypt");
+        mainSection.insertBefore(
+            errorMessage,
+            document.querySelector(".encrypt-decrypt__user-action"));
+        return;
+    }
+
+    let decryptedText = "";
+    let i = 0;
+
+    while (i < text.length) {
+        if (text.substring(i, i + 2) === "ai")
+        {
+            decryptedText += "a";
+            i += 2;
+        }
+        else if (text.substring(i, i + 5) === "enter")
+        {
+            decryptedText += "e";
+            i += 5;
+        }
+        else if (text.substring(i, i + 4) === "imes")
+        {
+            decryptedText += "i";
+            i += 4
+        }
+        else if (text.substring(i, i + 4) === "ober")
+        {
+            decryptedText += "o";
+            i += 4;
+        }
+        else if (text.substring(i, i + 4) === "ufat")
+        {
+            decryptedText += "u";
+            i += 4;
+        }
+        else
+        {
+            decryptedText += text[i];
+            i++;
+        }
+    }
+
+    const userOutput = document.querySelector("#user-output");
+    userOutput.textContent = decryptedText;
+}
 
 function clearTextArea() {}
 
