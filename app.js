c859b7fa51dd20c1ad5822591ae7ea6d121e630b
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-08 00:03:11 -0300

    Implemented copyToClipboard function for copying text to the clipboard

--- app.js
+++ app.js
@@ -22,18 +22,12 @@ function encryptText() {
   let encryptedText = "";
 
   for (let i = 0; i < text.length; i++) {
-    if (text[i] == "a")
-        encryptedText += "ai";
-    else if (text[i] == "e")
-        encryptedText += "enter";
-    else if (text[i] == "i")
-        encryptedText += "imes";
-    else if (text[i] == "o")
-        encryptedText += "ober";
-    else if (text[i] == "u")
-        encryptedText += "ufat";
-    else
-        encryptedText += text[i];
+    if (text[i] == "a") encryptedText += "ai";
+    else if (text[i] == "e") encryptedText += "enter";
+    else if (text[i] == "i") encryptedText += "imes";
+    else if (text[i] == "o") encryptedText += "ober";
+    else if (text[i] == "u") encryptedText += "ufat";
+    else encryptedText += text[i];
   }
   const userOutput = document.querySelector("#user-output");
   userOutput.textContent = encryptedText;
@@ -45,58 +39,58 @@ function decryptText() {
     2. Text should not contain any special characters or letters with accents
     8. Text should not contain lower case letters
     */
-    let text = document.querySelector("textarea").value;
+  let text = document.querySelector("textarea").value;
 
-    if (/[^a-z0-9]/.test(text)) {
-        const errorMessage = document.createElement("p");
-        errorMessage.textContent =
-        "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
-        errorMessage.classList.add("error-message");
-        const mainSection = document.querySelector(".encrypt-decrypt");
-        mainSection.insertBefore(
-            errorMessage,
-            document.querySelector(".encrypt-decrypt__user-action"));
-        return;
+  if (/[^a-z0-9]/.test(text)) {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent =
+      "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
+    errorMessage.classList.add("error-message");
+    const mainSection = document.querySelector(".encrypt-decrypt");
+    mainSection.insertBefore(
+      errorMessage,
+      document.querySelector(".encrypt-decrypt__user-action")
+    );
+    return;
+  }
+
+  let decryptedText = "";
+  let i = 0;
+
+  while (i < text.length) {
+    if (text.substring(i, i + 2) === "ai") {
+      decryptedText += "a";
+      i += 2;
+    } else if (text.substring(i, i + 5) === "enter") {
+      decryptedText += "e";
+      i += 5;
+    } else if (text.substring(i, i + 4) === "imes") {
+      decryptedText += "i";
+      i += 4;
+    } else if (text.substring(i, i + 4) === "ober") {
+      decryptedText += "o";
+      i += 4;
+    } else if (text.substring(i, i + 4) === "ufat") {
+      decryptedText += "u";
+      i += 4;
+    } else {
+      decryptedText += text[i];
+      i++;
     }
+  }
 
-    let decryptedText = "";
-    let i = 0;
-
-    while (i < text.length) {
-        if (text.substring(i, i + 2) === "ai")
-        {
-            decryptedText += "a";
-            i += 2;
-        }
-        else if (text.substring(i, i + 5) === "enter")
-        {
-            decryptedText += "e";
-            i += 5;
-        }
-        else if (text.substring(i, i + 4) === "imes")
-        {
-            decryptedText += "i";
-            i += 4
-        }
-        else if (text.substring(i, i + 4) === "ober")
-        {
-            decryptedText += "o";
-            i += 4;
-        }
-        else if (text.substring(i, i + 4) === "ufat")
-        {
-            decryptedText += "u";
-            i += 4;
-        }
-        else
-        {
-            decryptedText += text[i];
-            i++;
-        }
-    }
-
-    const userOutput = document.querySelector("#user-output");
-    userOutput.textContent = decryptedText;
+  const userOutput = document.querySelector("#user-output");
+  userOutput.textContent = decryptedText;
 }
 
-function copyText() {}
+async function copyToClipboard() {
+  const userOutput = document.querySelector("#user-output").textContent;
+
+  try {
+    await navigator.clipboard.writeText(userOutput);
+    console.log("Copiado");
+  } catch (err)
+  {
+    console.log("Falha ao copiar mensagem");
+  }
+}
