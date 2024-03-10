ee8dea91b0e71862eb1e37466a3edb951c7c2ee8
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-10 10:35:49 -0300

    Made indentation improvements for better readability

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-function encryptText() {
-  /* RULES:
-  1. The tag should be a valid HTML
-  2. Text should not contain any special characters or letters with accents
-  8. Text should not contain lower case letters
-  */
+function encryptText()
+{
   let text = document.querySelector("textarea").value;
 
-  if (/[^a-z0-9]/.test(text)) {
+  if (/[^a-z0-9]/.test(text))
+  {
     const errorMessage = document.createElement("p");
     errorMessage.textContent =
       "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
@@ -14,31 +11,33 @@ function encryptText() {
     const mainSection = document.querySelector(".encrypt-decrypt");
     mainSection.insertBefore(
       errorMessage,
-      document.querySelector(".encrypt-decrypt__user-action")
-    );
+      document.querySelector(".encrypt-decrypt__user-action"));
     return;
   }
 
   let encryptedText = "";
 
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] == "a") encryptedText += "ai";
-    else if (text[i] == "e") encryptedText += "enter";
-    else if (text[i] == "i") encryptedText += "imes";
-    else if (text[i] == "o") encryptedText += "ober";
-    else if (text[i] == "u") encryptedText += "ufat";
-    else encryptedText += text[i];
+  for (let i = 0; i < text.length; i++)
+  {
+    if (text[i] == "a")
+      encryptedText += "ai";
+    else if (text[i] == "e")
+      encryptedText += "enter";
+    else if (text[i] == "i")
+      encryptedText += "imes";
+    else if (text[i] == "o")
+      encryptedText += "ober";
+    else if (text[i] == "u")
+      encryptedText += "ufat";
+    else
+      encryptedText += text[i];
   }
   const userOutput = document.querySelector("#user-output");
   userOutput.textContent = encryptedText;
 }
 
-function decryptText() {
-  /* RULES:
-    1. The tag should be a valid HTML
-    2. Text should not contain any special characters or letters with accents
-    8. Text should not contain lower case letters
-    */
+function decryptText()
+{
   let text = document.querySelector("textarea").value;
 
   if (/[^a-z0-9]/.test(text)) {
@@ -49,31 +48,42 @@ function decryptText() {
     const mainSection = document.querySelector(".encrypt-decrypt");
     mainSection.insertBefore(
       errorMessage,
-      document.querySelector(".encrypt-decrypt__user-action")
-    );
+      document.querySelector(".encrypt-decrypt__user-action"));
     return;
   }
 
   let decryptedText = "";
   let i = 0;
 
-  while (i < text.length) {
-    if (text.substring(i, i + 2) === "ai") {
+  while (i < text.length)
+  {
+    if (text.substring(i, i + 2) === "ai")
+    {
       decryptedText += "a";
       i += 2;
-    } else if (text.substring(i, i + 5) === "enter") {
+    }
+    else if (text.substring(i, i + 5) === "enter")
+    {
       decryptedText += "e";
       i += 5;
-    } else if (text.substring(i, i + 4) === "imes") {
+    }
+    else if (text.substring(i, i + 4) === "imes")
+    {
       decryptedText += "i";
       i += 4;
-    } else if (text.substring(i, i + 4) === "ober") {
+    }
+    else if (text.substring(i, i + 4) === "ober")
+    {
       decryptedText += "o";
       i += 4;
-    } else if (text.substring(i, i + 4) === "ufat") {
+    }
+    else if (text.substring(i, i + 4) === "ufat")
+    {
       decryptedText += "u";
       i += 4;
-    } else {
+    }
+    else
+    {
       decryptedText += text[i];
       i++;
     }
@@ -83,14 +93,17 @@ function decryptText() {
   userOutput.textContent = decryptedText;
 }
 
-async function copyToClipboard() {
+async function copyToClipboard()
+{
   const userOutput = document.querySelector("#user-output").textContent;
 
-  try {
-    await navigator.clipboard.writeText(userOutput);
-    console.log("Copiado");
-  } catch (err)
+  try
   {
-    console.log("Falha ao copiar mensagem");
+    await navigator.clipboard.writeText(userOutput);
+    console.log("Copiado"); /* Delete this */
+  }
+  catch (err)
+  {
+    console.log("Falha ao copiar mensagem"); /* Delete this */
   }
 }
