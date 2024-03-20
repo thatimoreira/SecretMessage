16f0e2a8e420a8cf4123f00f308ce5146389b199
thatimoreira <thatimoreira.dev@gmail.com>
2024-03-20 19:34:43 -0300

    Changed the name of some params as I renamed some elements on the html file

--- app.js
+++ app.js
@@ -1,43 +1,4 @@
-function encryptText()
-{
-  let text = document.querySelector("textarea").value;
-
-  if (/[^a-z0-9]/.test(text))
-  {
-    const errorMessage = document.createElement("p");
-    errorMessage.textContent =
-      "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
-    errorMessage.classList.add("error-message");
-    const mainSection = document.querySelector(".encrypt-decrypt");
-    mainSection.insertBefore(
-      errorMessage,
-      document.querySelector(".encrypt-decrypt__user-action"));
-    return;
-  }
-
-  let encryptedText = "";
-
-  for (let i = 0; i < text.length; i++)
-  {
-    if (text[i] == "a")
-      encryptedText += "ai";
-    else if (text[i] == "e")
-      encryptedText += "enter";
-    else if (text[i] == "i")
-      encryptedText += "imes";
-    else if (text[i] == "o")
-      encryptedText += "ober";
-    else if (text[i] == "u")
-      encryptedText += "ufat";
-    else
-      encryptedText += text[i];
-  }
-  const userOutput = document.querySelector("#user-output");
-  userOutput.textContent = encryptedText;
-}
-
-function decryptText()
-{
+function encryptText() {
   let text = document.querySelector("textarea").value;
 
   if (/[^a-z0-9]/.test(text)) {
@@ -48,42 +9,61 @@ function decryptText()
     const mainSection = document.querySelector(".encrypt-decrypt");
     mainSection.insertBefore(
       errorMessage,
-      document.querySelector(".encrypt-decrypt__user-action"));
+      document.querySelector(".encrypt-decrypt__user-input")
+    );
+    return;
+  }
+
+  let encryptedText = "";
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] == "a") encryptedText += "ai";
+    else if (text[i] == "e") encryptedText += "enter";
+    else if (text[i] == "i") encryptedText += "imes";
+    else if (text[i] == "o") encryptedText += "ober";
+    else if (text[i] == "u") encryptedText += "ufat";
+    else encryptedText += text[i];
+  }
+  const userOutput = document.querySelector("#user-output");
+  userOutput.textContent = encryptedText;
+}
+
+function decryptText() {
+  let text = document.querySelector("textarea").value;
+
+  if (/[^a-z0-9]/.test(text)) {
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent =
+      "Não são aceitos: letras minúscula, acentos ou caracteres especiais";
+    errorMessage.classList.add("error-message");
+    const mainSection = document.querySelector(".encrypt-decrypt");
+    mainSection.insertBefore(
+      errorMessage,
+      document.querySelector(".encrypt-decrypt__user-input")
+    );
     return;
   }
 
   let decryptedText = "";
   let i = 0;
 
-  while (i < text.length)
-  {
-    if (text.substring(i, i + 2) === "ai")
-    {
+  while (i < text.length) {
+    if (text.substring(i, i + 2) === "ai") {
       decryptedText += "a";
       i += 2;
-    }
-    else if (text.substring(i, i + 5) === "enter")
-    {
+    } else if (text.substring(i, i + 5) === "enter") {
       decryptedText += "e";
       i += 5;
-    }
-    else if (text.substring(i, i + 4) === "imes")
-    {
+    } else if (text.substring(i, i + 4) === "imes") {
       decryptedText += "i";
       i += 4;
-    }
-    else if (text.substring(i, i + 4) === "ober")
-    {
+    } else if (text.substring(i, i + 4) === "ober") {
       decryptedText += "o";
       i += 4;
-    }
-    else if (text.substring(i, i + 4) === "ufat")
-    {
+    } else if (text.substring(i, i + 4) === "ufat") {
       decryptedText += "u";
       i += 4;
-    }
-    else
-    {
+    } else {
       decryptedText += text[i];
       i++;
     }
@@ -93,17 +73,13 @@ function decryptText()
   userOutput.textContent = decryptedText;
 }
 
-async function copyToClipboard()
-{
+async function copyToClipboard() {
   const userOutput = document.querySelector("#user-output").textContent;
 
-  try
-  {
+  try {
     await navigator.clipboard.writeText(userOutput);
     console.log("Copiado"); /* Delete this */
-  }
-  catch (err)
-  {
+  } catch (err) {
     console.log("Falha ao copiar mensagem"); /* Delete this */
   }
 }
